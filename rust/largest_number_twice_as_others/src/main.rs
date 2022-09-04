f553b93076911c5b7ca5ac584af4b272c28ba70a
Miroslav Kovar <miroslav.kovar@absa.africa>
2022-09-04 20:16:44 +0200

    Largest Number at Least Twice as Others
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

---
+++ rust/largest_number_twice_as_others/src/main.rs
@@ -0,0 +1,20 @@
+fn largest_number_twice_as_others(nums: Vec<i32>) -> i32 {
+    let mut max = nums[0];    
+    let mut j = 0;
+    for (i, n) in nums.iter().enumerate() {
+        if *n > max {
+            max = *n;
+            j = i;
+        }
+    }
+    for n in nums {
+        if 2 * n > max && n != max { return -1; }
+    }
+    j as i32
+}
+
+#[test]
+fn test_largest_number_twice_as_others() {
+    assert_eq!(largest_number_twice_as_others(vec![3,6,1,0]), 1);
+    assert_eq!(largest_number_twice_as_others(vec![1,2,3,4]), -1);
+}
