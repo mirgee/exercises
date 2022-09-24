37b6f3a7aacb91ff7f1d90aab076147138ec4c21
Miroslav Kovar <miroslav.kovar@absa.africa>
2022-09-24 21:08:35 +0200

    Reshape the Matrix
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

---
+++ rust/reshape_matrix/src/main.rs
@@ -0,0 +1,18 @@
+fn reshape_matrix(mat: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>> {
+    let r = r as usize;
+    let c = c as usize;
+    let rr = mat.len();
+    let cc = mat[0].len();
+    if r * c != rr * cc { return mat; }
+    let mut res: Vec<Vec<i32>> = vec![vec![0; c]; r];
+    for i in 0..r * c {
+        res[i / c][i % c] = mat[i / cc][i % cc];
+    }
+    res
+}
+
+#[test]
+fn test_reshape_matrix() {
+    assert_eq!(reshape_matrix(vec![vec![1,2],vec![3,4]], 1, 4), vec![vec![1,2,3,4]]);
+    assert_eq!(reshape_matrix(vec![vec![1,2],vec![3,4]], 2, 4), vec![vec![1,2],vec![3,4]]);
+}
