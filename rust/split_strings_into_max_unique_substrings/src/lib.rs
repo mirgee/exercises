0a8b9e21ab1526e1b8b3ccb2245854970509ba74
Miroslav Kovar <miroslav.kovar@absa.africa>
2022-12-27 19:24:39 +0100

    split_strings_into_max_unique_substrings
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

--- rust/split_strings_into_max_unique_substrings/src/lib.rs
+++ rust/split_strings_into_max_unique_substrings/src/lib.rs
@@ -1,6 +1,24 @@
 //https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/description/
+use std::collections::HashSet;
+use std::cmp::max;
 
-pub fn split_strings_into_max_unique_substrings() -> {
+fn _split_strings_into_max_unique_substrings(s: &str, ps: &mut HashSet<String>) -> i32 {
+    let mut subans = 0;
+    println!("ps: {:?}", ps);
+    for i in 1..s.len() + 1 {
+        let substr: String = s[..i].into();
+        if !ps.contains(&substr) {
+            ps.insert(substr.clone());
+            subans = max(subans, 1 + _split_strings_into_max_unique_substrings(&s[i..], ps));
+            ps.remove(&substr);
+        }
+    }
+    subans
+}
+
+pub fn split_strings_into_max_unique_substrings(s: String) -> i32 {
+    let mut ps = HashSet::new();
+    _split_strings_into_max_unique_substrings(s.as_str(), &mut ps)
 }
 
 #[cfg(test)]
@@ -9,8 +27,22 @@ mod tests {
 
     #[test]
     fn test_split_strings_into_max_unique_substrings_0() {
-        let expected = todo!();
-        let actual = split_strings_into_max_unique_substrings();
+        let expected = 5;
+        let actual = split_strings_into_max_unique_substrings("ababccc".into());
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_split_strings_into_max_unique_substrings_1() {
+        let expected = 2;
+        let actual = split_strings_into_max_unique_substrings("aba".into());
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_split_strings_into_max_unique_substrings_2() {
+        let expected = 1;
+        let actual = split_strings_into_max_unique_substrings("aa".into());
         assert_eq!(expected, actual);
     }
 }
