6d2b52dd4ed26b7fd44786bd91bd8d8b265adb5d
Miroslav Kovar <miroslav.kovar@absa.africa>
2022-12-27 17:49:30 +0100

    Add an interesting problem to work on split_strings_into_max_unique_substrings
    Signed-off-by: Miroslav Kovar <miroslav.kovar@absa.africa>

---
+++ rust/split_strings_into_max_unique_substrings/src/lib.rs
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/description/
+
+pub fn split_strings_into_max_unique_substrings() -> {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_split_strings_into_max_unique_substrings_0() {
+        let expected = todo!();
+        let actual = split_strings_into_max_unique_substrings();
+        assert_eq!(expected, actual);
+    }
+}
